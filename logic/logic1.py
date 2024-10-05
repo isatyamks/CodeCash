466514390618945f3e490d4d0b1b22902eb08e93
harshaaaaw <94235018+harshaaaaw@users.noreply.github.com>
2024-10-05 21:53:53 +0530

    Update logic1.py
    updated logic1.py file and fixed the flask error

--- logic/logic1.py
+++ logic/logic1.py
@@ -1,17 +1,18 @@
 from pymongo import MongoClient
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 client = MongoClient('mongodb://localhost:27017')
 
+# Database and collection setup
 db_login = client.login
 users_collection = db_login.users
 
 db_bank = client.bank
 bank_collection = db_bank.assets
+
+# Safely retrieve the bank assets record
 bank_assets_record = bank_collection.find_one({'_id': 'bank_assets'})
-bank_assets = int(bank_assets_record.get('total_assets', 0))
-
-
+bank_assets = int(bank_assets_record.get('total_assets', 0)) if bank_assets_record else 0
 
 def update_worth(username, amount, action):
     user = users_collection.find_one({'user_name': username})
@@ -20,7 +21,7 @@ def update_worth(username, amount, action):
 
     current_worth = user.get('worth', 0)
     bank_assets_record = bank_collection.find_one({'_id': 'bank_assets'})
-    bank_assets = int(bank_assets_record.get('total_assets', 0))
+    bank_assets = int(bank_assets_record.get('total_assets', 0)) if bank_assets_record else 0
     
     if action == 'deposit':
         new_worth = current_worth + amount
@@ -38,41 +39,39 @@ def update_worth(username, amount, action):
     bank_collection.update_one({'_id': 'bank_assets'}, {'$set': {'total_assets': new_bank_assets}})
     return "Transaction successful"
 
-
-
-
 def next_month(username):
     income = 600
     expenditure = 500
     user = users_collection.find_one({'user_name': username})
-    bank_assets_record = bank_collection.find_one({'_id': 'bank_assets'})
-    bank_assets=bank_assets_record.get('total_assets',0)
     if not user:
         return
     
+    bank_assets_record = bank_collection.find_one({'_id': 'bank_assets'})
+    bank_assets = bank_assets_record.get('total_assets', 0) if bank_assets_record else 0
+
     current_worth = user.get('worth', 0)
     fd_amount = user.get('fd', 0)
     loan_amount = user.get('loan', 0)
-    loan_time = user.get('loan_time', 0)
-    loan_time = int(loan_time)
-    new_bank_assets=bank_assets
-    new_worth = current_worth  
-    new_loan_amount = loan_amount  
+    loan_time = int(user.get('loan_time', 0))
+
+    new_bank_assets = bank_assets
     new_worth = current_worth + income - expenditure
+    new_loan_amount = loan_amount
+
+    # Process loan payments if any
     if loan_amount > 0:
         if loan_time > 0:
-            
-            
-            
             loan_time -= 1
         else:
-            
-            new_worth -= loan_amount * 0.1
-            new_bank_assets +=bank_assets*0.1
-            new_loan_amount = loan_amount - loan_amount * 0.1
-    
+            repayment = loan_amount * 0.1
+            new_worth -= repayment
+            new_bank_assets += repayment
+            new_loan_amount = loan_amount - repayment
+
+    # Update FD earnings
     new_worth += fd_amount * 0.05
-    new_bank_assets -= fd_amount *0.05
+    new_bank_assets -= fd_amount * 0.05
+
     current_date = user.get('current_date', datetime.now())
     new_date = current_date + timedelta(days=30)
     
@@ -83,32 +82,33 @@ def next_month(username):
                 'worth': new_worth,
                 'current_date': new_date,
                 'loan': new_loan_amount,
-                'loan_time': loan_time  
+                'loan_time': loan_time
             }
         }
     )
     bank_collection.update_one({'_id': 'bank_assets'}, {'$set': {'total_assets': new_bank_assets}})
-    
     print(new_date)
-
-
-
-
 
 def fd(username, amount):
     user = users_collection.find_one({'user_name': username})
     if not user:
         return
+
     current_worth = user.get('worth', 0)
     fd_amount = int(amount)
-    current_fd_amount = user.get('fd', 0)
-    if current_worth<fd_amount:
+
+    if current_worth < fd_amount:
         return
+
+    current_fd_amount = user.get('fd', 0)
     new_fd_amount = current_fd_amount + fd_amount
+
     bank_assets_record = bank_collection.find_one({'_id': 'bank_assets'})
-    bank_assets=bank_assets_record.get('total_assets',0)
-    new_bank_assets=bank_assets+fd_amount
+    bank_assets = bank_assets_record.get('total_assets', 0) if bank_assets_record else 0
+    new_bank_assets = bank_assets + fd_amount
+
     new_worth = current_worth - fd_amount
+
     users_collection.update_one(
         {'user_name': username},
         {
@@ -120,17 +120,24 @@ def fd(username, amount):
     )
     bank_collection.update_one({'_id': 'bank_assets'}, {'$set': {'total_assets': new_bank_assets}})
 
-def rd(username, amount,time_period):
+# Similar changes applied to rd, sip, lumpsum, and loan functions
+
+def rd(username, amount, time_period):
     user = users_collection.find_one({'user_name': username})
     if not user:
         return
+
     current_worth = user.get('worth', 0)
     fd_amount = int(amount)
-    current_fd_amount = user.get('fd', 0)
-    if current_worth<fd_amount:
+
+    if current_worth < fd_amount:
         return
+
+    current_fd_amount = user.get('fd', 0)
     new_fd_amount = current_fd_amount + fd_amount
+
     new_worth = current_worth - fd_amount
+
     users_collection.update_one(
         {'user_name': username},
         {
@@ -145,13 +152,18 @@ def sip(username, amount):
     user = users_collection.find_one({'user_name': username})
     if not user:
         return
+
     current_worth = user.get('worth', 0)
     fd_amount = int(amount)
-    current_fd_amount = user.get('fd', 0)
-    if current_worth<fd_amount:
+
+    if current_worth < fd_amount:
         return
+
+    current_fd_amount = user.get('fd', 0)
     new_fd_amount = current_fd_amount + fd_amount
+
     new_worth = current_worth - fd_amount
+
     users_collection.update_one(
         {'user_name': username},
         {
@@ -166,11 +178,14 @@ def lumpsum(username, amount):
     user = users_collection.find_one({'user_name': username})
     if not user:
         return
+
     current_worth = user.get('worth', 0)
     lumpsum_amount = int(amount)
-    current_lumpsum_amount = user.get('fd', 0)
-    new_lumpsum_amount = current_lumpsum_amount + lumpsum_amount
+
     new_worth = current_worth - lumpsum_amount
+    current_lumpsum_amount = user.get('lumpsum', 0)
+    new_lumpsum_amount = current_lumpsum_amount + lumpsum_amount
+
     users_collection.update_one(
         {'user_name': username},
         {
@@ -181,25 +196,30 @@ def lumpsum(username, amount):
         }
     )
 
-def loan(username, amount,time_period):
+def loan(username, amount, time_period):
     user = users_collection.find_one({'user_name': username})
     if not user:
         return
+
     current_worth = user.get('worth', 0)
     loan_amount = int(amount)
+
     current_loan_amount = user.get('loan', 0)
     new_loan_amount = current_loan_amount + loan_amount
+
     bank_assets_record = bank_collection.find_one({'_id': 'bank_assets'})
-    bank_assets=bank_assets_record.get('total_assets',0)
-    new_bank_assets=bank_assets-loan_amount
+    bank_assets = bank_assets_record.get('total_assets', 0) if bank_assets_record else 0
+    new_bank_assets = bank_assets - loan_amount
+
     new_worth = current_worth + loan_amount
+
     users_collection.update_one(
         {'user_name': username},
         {
             '$set': {
                 'worth': new_worth,
                 'loan': new_loan_amount,
-                'loan_time':time_period
+                'loan_time': time_period
             }
         }
     )
